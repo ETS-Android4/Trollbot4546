2216b24416ec43c508086355f95c40baa90487d3
Ilgneous <65877504+Ilgneous@users.noreply.github.com>
2022-03-23 14:43:15 -0500

    gradle lolz

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TrollbotTeleOp.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@TeleOp(name = "Trollbot2021", group = "TeleOp")
+public class TrollbotTeleOp extends OpMode  {
+
+    ElapsedTime time = new ElapsedTime();
+    public void loop() {
+
+    }
+}
